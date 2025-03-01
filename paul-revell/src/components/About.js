c40dcc32c0aedd8029c81540c93481365bb596e8
Paul <101579244+PaulRevell@users.noreply.github.com>
2025-03-01 23:50:17 +0000

    Updated text wrapping and current role

--- paul-revell/src/components/About.js
+++ paul-revell/src/components/About.js
@@ -11,12 +11,15 @@ const About = () => {
         <div className="about-text">
           <h2>About Me</h2>
           <p>
-            Recent Computer Science graduate from the University of St Andrews (2024) based in London. I have 
-            a strong interest in different kinds of technologies and developing software. I mostly use Java, 
-            Python, C, and React in both my own and my university projects and am keen on learning more 
-            languages as required. In my free time, I like to develop video games and software ideas that I think 
-            of. I am currently working on a few systems and games as side-projects. Skill-set: Full-stack, SCRUM, 
-            object-oriented design, algorithm design, data structures, problem solving and complexity analysis, teamwork.
+            I'm currently working as a Data Analyst and have recently graduated with a Computer Science degree from The University of St Andrews (2024). I have 
+            a strong interest in different kinds of technologies and developing both software and games. I primarily use Java, 
+            Python, C, C#, and React which I have used in both personal and university projects and am keen on learning more languages as required.
+            In my spare time, I love to develop many different genres of video games and working on new software ideas.
+            <br/><br/>
+            In my current role as a Data Analyst, I build queries to support and justify data-driven decisions throughout different teams in the company. I also create
+            various data visualisations in Tableau and MATLAB and Excel that are accessible and useful to team members in various roles.
+            <br/><br/>
+            Skill-set: Full-stack, Tableau, SCRUM, object-oriented design, algorithm design, data structures, problem solving and complexity analysis.
           </p>
         </div>
       </div>
